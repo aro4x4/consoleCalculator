3a52431a5ad31b815985a9478ffdaf80ed6c59f5
aro4x4 <vlad2804@yahoo.com>
2022-06-23 20:21:38 +0300

    changed to float

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@
         {
             string operation;
             Console.WriteLine("CALCULATOR");
+            Console.WriteLine("Enter first operand:");
+            float operand_1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter operator + , - , * , / ");
             operation = Console.ReadLine();
-            Console.WriteLine("Enter first operand:");
-            int operand_1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter second operand:");
-            int operand_2 = Convert.ToInt32(Console.ReadLine());
-            int result = 0;
+            float operand_2 = Convert.ToInt32(Console.ReadLine());
+            float result = 0;
             switch(operation)
             {
                 case "+": {
@@ -39,24 +39,24 @@
             Console.ReadKey();
         }
 
-        public static int Addition(int operand_1, int operand_2)
+        public static float Addition(float operand_1, float operand_2)
         {
-            int result = operand_1 + operand_2;
+            float result = operand_1 + operand_2;
             return result;
         }
-        public static int Substraction(int operand_1, int operand_2)
+        public static float Substraction(float operand_1, float operand_2)
         {
-            int result = operand_1 - operand_2;
+            float result = operand_1 - operand_2;
             return result;
         }
-        public static int Multiplication(int operand_1, int operand_2)
+        public static float Multiplication(float operand_1, float operand_2)
         {
-            int result = operand_1 * operand_2;
+            float result = operand_1 * operand_2;
             return result;
         }
-        public static int Division(int operand_1, int operand_2)
+        public static float Division(float operand_1, float operand_2)
         {
-            int result = operand_1 / operand_2;
+            float result = operand_1 / operand_2;
             return result;
         }
     }
